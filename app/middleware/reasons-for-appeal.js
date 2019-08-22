42199adbb40c375befb929e2e00ad469e263e52a
chrisgrimble <chris.grimble@hmcts.net>
2019-08-22 14:46:18 +0100

    Changed reason for appeal to be max 250 words not 250 characters.

--- app/middleware/reasons-for-appeal.js
+++ app/middleware/reasons-for-appeal.js
@@ -2,9 +2,13 @@ const { formController } = require('./form-controller');
 const { check } = require('express-validator');
 
 function validation(locale) {
+  const maxNumberOfWords = 250;
   return [
     check('why').isLength({ min: 1 }).withMessage(locale.reasonsForAppeal.errors.tooShort)
-      .isLength({ max: 250 }).withMessage(locale.reasonsForAppeal.errors.tooLong)
+      .custom(value => {
+        const numbrOfWords = value.trim().split(' ');
+        return numbrOfWords.length <= maxNumberOfWords;
+      }).withMessage(locale.reasonsForAppeal.errors.tooLong)
   ];
 }
 
