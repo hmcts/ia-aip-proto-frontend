31b586f44ba1d92036705664198e7786d0b09719
chrisgrimble <chris.grimble@hmcts.net>
2019-09-06 16:49:08 +0100

    Enable review and submit answers links when you have answered all the questions

--- app/middleware/case-building/questions-from-tribunal.js
+++ app/middleware/case-building/questions-from-tribunal.js
@@ -1,7 +1,13 @@
 module.exports = (req, res) => {
+  const hasAnsweredAllQuestion = req.session.appealData.questions.reduce(
+    (lastValue, question) => lastValue && question.completed,
+    true
+  );
+
   res.render('case-building/questions-from-tribunal.html', {
     hideBackLink: false,
     previousPage: '/case-building/overview',
-    appealData: req.session.appealData
+    appealData: req.session.appealData,
+    hasAnsweredAllQuestion
   });
 };
