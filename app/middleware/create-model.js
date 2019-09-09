e79818817a00aceff9e2145840a880b9369661ad
chrisgrimble <chris.grimble@hmcts.net>
2019-09-09 16:13:30 +0100

    Added ability to create data in model for all steps of the process.

--- app/middleware/create-model.js
+++ app/middleware/create-model.js
@@ -49,7 +49,7 @@ module.exports = (req, res, next) => {
   }
 
   if (req.query.createData) {
-    req.session.appealData = {
+    Object.assign(req.session.appealData, {
       yourDetails: {
         homeOffice: {
           completed: true,
@@ -79,7 +79,7 @@ module.exports = (req, res, next) => {
           },
           text: {
             selected: true,
-            phoneNumber: '07899948907'
+            phoneNumber: '07899999999'
           },
           post: {
             selected: true
@@ -103,7 +103,25 @@ module.exports = (req, res, next) => {
         checkAnswers: {
           completed: false
         }
-      },
+      }
+    });
+  }
+
+  if (req.query.reasonsForAppeal) {
+    Object.assign(req.session.appealData, {
+      appealDetails: {
+        reasonsForAppeal: {
+          completed: true,
+          // eslint-disable-next-line max-len
+          why: 'This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so te lar.This is a reason for the appeal.Foo bar so.',
+          evidence: []
+        }
+      }
+    });
+  }
+
+  if (req.query.questions) {
+    Object.assign(req.session.appealData, {
       questions: [
         {
           title: 'Tell us more about your children',
@@ -112,19 +130,21 @@ module.exports = (req, res, next) => {
             '* What are their names?\n' +
             '* What are their ages?\n' +
             '* How long have they lived in the UK?',
-          completed: false
+          completed: true,
+          // eslint-disable-next-line max-len
+          answer: 'My children\'s names are Ali, Umid and Ghulam. Ali is 8, Umiod is 6 and Ghulam is 2. They have lived in the UK since they were born.',
+          evidence: ['evidence.txt']
         },
         {
           title: 'Some pages from the country report you submitted as evidence are missing',
           description: 'Pages 5 to 7 from the country report are missing.\n\nPlease provide the missing pages below.',
-          completed: false
+          completed: true,
+          answer: 'I have attached the missing pages',
+          evidence: ['page5.png', 'page6.png', 'page7.png']
         }
-      ],
-      // eslint-disable-next-line no-magic-numbers
-      respondByDate: moment.utc().add(1, 'weeks').format('D MMMM YYYY')
-    };
+      ]
+    });
   }
-
 
   next();
 };
