173af207f8cd45b18b9aff0e420bd5d31d7e17e8
Toni Sanchez <toninos@gmail.com>
2019-09-13 15:08:05 +0100

    TCW clarifying questions journey (#12)

--- app/ccd-routes.js
+++ app/ccd-routes.js
@@ -1,39 +1,34 @@
 const express = require('express');
+const { check } = require('express-validator');
 const paths = require('./paths');
-const stages = require('./data/tcwStages');
+
+const {
+  getLandingPage,
+  getCcdList,
+  getCcdCaseOverview,
+  getCcdQuestions,
+  postCcdQuestions,
+  getReviewClarifyingQuestions,
+  getEditClarifyingQuestion,
+  postEditClarifyingQuestion,
+  getConfirmationClarifyingQuestions
+} = require('./controllers/ccdController');
 
 /* eslint-disable-next-line new-cap */
 const router = express.Router({});
 
-function getLandingPage(req, res) {
-  return res.redirect(`${paths.ccd}${paths.ccdCasesList}`);
-}
-
-function getCcdList(req, res) {
-  return res.render('ccd/index.html');
-}
-
-function getCcdCaseOverview(req, res) {
-  return res.render('ccd/overview.html', { stages });
-}
-
-function getCcdQuestions(req, res) {
-  const questions = req.session.appealData.questions || [];
-  return res.render('ccd/question.html', { questions });
-}
-
-function postCcdQuestions(req, res) {
-  const previousQuestions = req.session.appealData.questions || [];
-  req.session.appealData = {
-    questions: [...previousQuestions, { title: 'title', description: req.body['further-question'], completed: false }]
-  };
-  return res.redirect('questions');
+function validations() {
+  return [ check('further-question').not().isEmpty() ];
 }
 
 router.get('/', getLandingPage);
 router.get(paths.ccdCasesList, getCcdList);
 router.get(paths.ccdCaseOverview, getCcdCaseOverview);
-router.get(paths.ccdQuestions, getCcdQuestions);
-router.post(paths.ccdQuestions, postCcdQuestions);
+router.get(paths.ccdClarifyingQuestions, getCcdQuestions);
+router.post(paths.ccdClarifyingQuestions, validations(), postCcdQuestions);
+router.get(paths.ccdClarifyingQuestionsReview, getReviewClarifyingQuestions);
+router.get(paths.ccdClarifyingQuestionsEdit, getEditClarifyingQuestion);
+router.post(paths.ccdClarifyingQuestionsEdit, postEditClarifyingQuestion);
+router.get(paths.ccdClarifyingQuestionsConfirmation, getConfirmationClarifyingQuestions);
 
 module.exports = router;
