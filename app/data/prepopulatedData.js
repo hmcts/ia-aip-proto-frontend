ae07c9aac5d1fccee5996a84abc149529eb68c06
Toni sanchez <toninos@gmail.com>
2019-09-17 10:02:00 +0100

    adding title and description to case management appointment review page

--- app/data/prepopulatedData.js
+++ app/data/prepopulatedData.js
@@ -9,8 +9,8 @@ const directionsPrepopulated = {
   Please email cma@hmcts.net if you will need a translator to be at the meeting. This is free.
   
   You can bring a family member, friend or other supporter with you at the meeting.`,
-  respondent: `The case worker has invited the appellant in this case to a case management appointment. The purpose
-  of this appointment is to enable the appellant to ready their case for review.
+  // eslint-disable-next-line max-len
+  respondent: `The case worker has invited the appellant in this case to a case management appointment. The purpose of this appointment is to enable the appellant to ready their case for review.
   
   You are invited to attend the appointment. The details are below.`
 };
