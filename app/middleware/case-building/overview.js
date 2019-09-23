22a6e57b30615c0db2bf85706922c11826a42232
chrisgrimble <chris.grimble@hmcts.net>
2019-09-23 15:13:30 +0100

    Content changes for the case building questions

--- app/middleware/case-building/overview.js
+++ app/middleware/case-building/overview.js
@@ -2,6 +2,10 @@ const stages = require('../../data/appealStages');
 
 module.exports = (req, res) => {
   const reasonsForAppealCompleted = req.session.appealData.appealDetails.reasonsForAppeal.completed;
+  if (reasonsForAppealCompleted) {
+    stages[1].ticked = true;
+    stages[2].active = true;
+  }
 
   res.render('case-building/overview.html', {
     hideBackLink: true,
