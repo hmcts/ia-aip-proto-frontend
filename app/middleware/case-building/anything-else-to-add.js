bee56e2be0e2130566a27a978fa7ff0d02b171c0
chrisgrimble <chris.grimble@hmcts.net>
2019-09-24 10:15:51 +0100

    Fix error message

--- app/middleware/case-building/anything-else-to-add.js
+++ app/middleware/case-building/anything-else-to-add.js
@@ -11,6 +11,9 @@ function post(req, res) {
   if (req.body.anythingElseToAdd === 'yes') {
     res.redirect(paths.anythingElseToAddQuestion);
   }
+  if (!req.session.appealData.anythingElseToAdd) {
+    req.session.appealData.anythingElseToAdd = {};
+  }
   req.session.appealData.anythingElseToAdd.completed = true;
   res.redirect(paths.questionCheckAnswers);
 }
