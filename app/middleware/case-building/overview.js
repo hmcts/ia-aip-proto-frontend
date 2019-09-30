103cf9870a5fcd3a959e9779d2078a071e9bdf3c
chrisgrimble <chris.grimble@hmcts.net>
2019-09-30 17:06:41 +0100

    Change progress bar on overview for case building.

--- app/middleware/case-building/overview.js
+++ app/middleware/case-building/overview.js
@@ -3,10 +3,6 @@ const stages = require('../../data/appealStages');
 module.exports = (req, res) => {
   const userStages = JSON.parse(JSON.stringify(stages));
   const reasonsForAppealCompleted = req.session.appealData.appealDetails.reasonsForAppeal.completed;
-  if (reasonsForAppealCompleted) {
-    userStages[1].ticked = true;
-    userStages[2].active = true;
-  }
 
   res.render('case-building/overview.html', {
     hideBackLink: true,
