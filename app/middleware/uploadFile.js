8389ba3343c820897b61e18a09a44051c973b1bb
Lewis Williams <lewis.williams@transformuk.com>
2019-10-19 17:01:16 +0100

    Added file upload

--- app/middleware/uploadFile.js
+++ app/middleware/uploadFile.js
@@ -1,8 +1,18 @@
 
 module.exports = (req, res) => {
   const obj = {
-    name: req.body.file,
-    whatIsIt: 'Document'
+    file: req.body.file,
+    whatIsIt: req.body['file-upload']
   };
   res.render('appeal-out-of-time.html', obj);
 };
+
+// function getCcdQuestions(req, res) {
+//   if (req.query['edit-question']) {
+//     const questionId = req.query['edit-question'];
+//     const question = req.session.appealData.questions[questionId];
+//     return res.render('ccd/edit-clarifying-questions.html', { question });
+//   }
+//   const questions = req.session.appealData.questions || [];
+//   return res.render('ccd/new-clarifying-questions.html', { questions });
+// }
